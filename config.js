fbd4bc4bc4689863c79589367c1836c9561edc1b
Sumanth M <92622011+sumansam312@users.noreply.github.com>
2023-11-29 23:06:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,103 @@
+window.PorfolioConfig = {
+  name: "Sumanth",
+  jobTitle: "Ethical Hacker",
+  jobDescription:
+    "In the matrix of ones and zeros, ethical hackers are the architects of order, scripting a saga of protection in the face of digital chaos.",
+  skills: [
+    {
+      name: "Bash/Shell Scripting",
+      description:
+        "Scripting, automating routine tasks and optimizing workflows for enhanced system efficiency.",
+    },
+    {
+      name: "Python",
+      description:
+        "Leveraging its versatility and readability to develop efficient solutions for diverse applications.",
+    },
+    {
+      name: "C Programming",
+      description:
+        "Demonstrating strong problem-solving skills and efficient algorithmic implementations",
+    },
+    {
+      name: "javaScript",
+      description:
+        "Dynamic scripting to create interactive and dynamic web applications.",
+    },
+    {
+      name: "HTML",
+      description:
+        "Utilizing strong markup skills to create and enhance web content for seamless user experiences.",
+    },
+    {
+      name: "CSS",
+      description:
+        "Crafting visually appealing and responsive web designs, ensuring a seamless user interface",
+    },
+    {
+      name: "Materialize",
+      description:
+        "Materialize CSS framework, applying modern design principles for creating visually appealing and responsive user interfaces.",
+    },
+
+    {
+      name: "Tailwindcss",
+      description:
+        "For efficient and utility-first styling, streamlining the process of building modern and responsive user interfaces.",
+    },
+    {
+      name: "Django",
+      description:
+        "Building scalable and robust web applications with a focus on rapid development",
+    },
+  ],
+  works: [
+    {
+      name: "Python IOT Platform",
+      link: "#",
+      description:
+        "Building a Python-powered IoT (Internet of Things) platform involves creating a system that connects and manages IoT devices, collects and processes data from these devices, and provides a means for users to interact with and visualize the data. ",
+    },
+    {
+      name: "Virtual Power Electronics Lab",
+      link: "#",
+      description:
+        "A virtual power electronics lab is an online simulation environment that allows students and professionals to experiment with and learn about power electronics without the need for physical hardware. In this virtual setting, users can interact with simulated circuits, components, and control systems to gain hands-on experience in designing and analyzing power electronic systems.",
+    },
+  ],
+  hobbies: [
+    {
+      name: "Programming and Scripting",
+      description:
+        "Enhance your coding skills by learning new programming languages or mastering scripting languages like Python,  or Bash. This can be both enjoyable and beneficial for the work.",
+    },
+    {
+      name: "Follow Security Blogs and Forums",
+      description:
+        "Stay updated on the latest tools and techniques by following security blogs, forums, and social media channels.",
+    },
+    {
+      name: "Podcasts and Webinars",
+      description:
+        "Listen to podcasts or attend webinars where experts discuss and demonstrate the use of various tools. This can be a convenient way to learn about tools while on the go.",
+    },
+  ],
+  contacts: {
+    email: "sumanth.m70@proton.me",
+    mobileNo: "github.com/sumansam312",
+    links: [
+      {
+        name: "GitHub",
+        link: "https://github.com/sumansam312",
+      },
+      {
+        name: "Facebook",
+        link: "https://www.facebook.com/sumanth.sumu.3154",
+      },
+      {
+        name: "Twitter",
+        link: "https://twitter.com/Suman_s_a_m",
+      },
+    ],
+  },
+};
