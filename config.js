6dd731bf3b1e7f5f7f3ddaa88f89ed5e40942c90
Sumanth M <92622011+sumansam312@users.noreply.github.com>
2024-11-09 19:36:20 +0530

    Pentesting Portfolio updated

---
+++ config.js
@@ -0,0 +1,114 @@
+window.PorfolioConfig = {
+  name: "Sumanth",
+  jobTitle: "Ethical Hacker and Pentester",
+  jobDescription:
+    "In the matrix of ones and zeros, ethical hackers are the architects of order, scripting a saga of protection in the face of digital chaos.",
+  skills: [
+    {
+      name: "Python",
+      description:
+        "Leveraging its versatility and readability to develop efficient solutions for diverse applications.",
+    },
+    {
+      name: "Bash/Shell Scripting",
+      description:
+        "Scripting, automating routine tasks and optimizing workflows for enhanced system efficiency.",
+    },
+  ],
+  works: [
+    {
+      name: "Windows",
+      link: "#",
+      description:
+        "Windows OS is a crucial platform in corporate environments, presenting significant opportunities and challenges for ethical hackers conducting penetration testing to identify and mitigate vulnerabilities.",
+    },
+    {
+      name: "Parrot OS",
+      link: "#",
+      description:
+        "Parrot OS is a security-focused Linux distribution tailored for penetration testing and digital forensics, offering a comprehensive suite of tools and privacy features for ethical hackers.",
+    },
+    {
+      name: "Kali Linux",
+      link: "#",
+      description:
+        "Kali Linux is a premier penetration testing distribution with a wide range of pre-installed security tools, offering flexibility and customization for effective vulnerability assessments.",
+    },
+    {
+      name: "Black Arch",
+      link: "#",
+      description:
+        "BlackArch is a lightweight Linux distribution tailored for penetration testing, featuring over 2,000 tools for ethical hackers conducting comprehensive security assessments.",
+    },
+    {
+      name: "Tails OS",
+      description:
+        "Tails OS is a privacy-focused Linux distribution that runs from a USB stick, ensuring anonymity by routing all internet traffic through the Tor network for secure browsing.",
+    },
+  ],
+
+  hobbies: [
+    {
+      name: "TCP/IP, DNS, DHCP, HTTP, SSH, FTP",
+      description:
+        "Strong understanding of network protocols and their associated vulnerabilities, facilitating the identification of security weaknesses and potential exploit points in penetration testing engagements.",
+    },
+  ],
+
+  Pentesting: [
+    {
+      name: "Nmap, Wireshark, Recon-ng, Aircrack-ng, Metasploit, John the Ripper, Burpsuite",
+      description:
+        " Experienced in utilizing various software applications to identify, exploit, and remediate vulnerabilities in systems, networks, and applications, enhancing overall security posture.",
+    },
+  ],
+
+  Visual: [
+    {
+      name: "VMware, Virtualbox, Git, Github",
+      description:
+        "Proficient in VMware and VirtualBox for creating virtual environments, with skills in Git for version control and GitHub for code collaboration and project management.",
+    },
+  ],
+
+  Projects: [
+    {
+      name: "AuditAnalyzer",
+      link: "https://github.com/sumansam312/AuditAnalyzer",
+      description:
+        "The AuditAnalyzer project provides comprehensive system auditing and monitoring capabilities for system administrators and users. It aggregates essential system information, user activity logs, network statistics, and performance metrics, allowing users to maintain a secure and efficient operating environment.",
+    },
+
+    {
+      name: "IP-MAC-Tools",
+      link: "https://github.com/sumansam312/IP-MAC-Tools",
+      description:
+        "Efficient and easy-to-use network management tools for calculating subnets and identifying devices by MAC address, accessible on both mobile and desktop browsers.",
+    },
+    {
+      name: "TryHackMe",
+      link: "https://tryhackme.com/r/p/SumanthSam",
+      description:
+        "Built a solid foundation in ethical hacking by completing TryHackMe challenges focused on Linux security, OSINT techniques, and network exploitation, sharpening my ability to perform risk assessments and security audits.",
+    },
+  ],
+
+  contacts: {
+    email: "sumanth.m70@proton.me",
+    mobileNo: "github.com/sumansam312",
+    links: [
+      {
+        name: "GitHub",
+        link: "https://github.com/sumansam312",
+      },
+      {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/sumanth321/",
+      },
+      {
+        name: "X",
+        link: "https://twitter.com/Suman_s_a_m",
+      },
+    ],
+  },
+};
